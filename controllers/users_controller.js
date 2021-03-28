47bbc8fd54ac1588ed66a715b0cac277c93b7775
shivangi <tiwarishivangi46@gmail.com>
2021-03-28 11:28:01 -0600

    setting up the files and directories

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,10 @@
-module.exports.profile = function(req,res){
-    console.log("Inside Profile Controller");
-    return res.end('<h1> here i m </h1>');
+// module.exports.profile = function(req,res){
+//     console.log("Inside Profile Controller");
+//     return res.end('<h1> here i m </h1>');
+// }
+
+module.exports.profile = function(req, res){
+    return res.render('users_profile', {
+        title: 'User Profile'
+    })
 }
